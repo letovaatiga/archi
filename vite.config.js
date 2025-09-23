b4cca89fc680fb5f0d2e8055267bec9ec6415033
letovaatiga <58217808+letovaatiga@users.noreply.github.com>
2025-09-23 13:46:18 +0400

    test

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/archi/',
+  base: '',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
