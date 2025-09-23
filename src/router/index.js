95cd34745a9aacef4ecfe030177f3bfd9efb2f4c
letovaatiga <58217808+letovaatiga@users.noreply.github.com>
2025-09-23 14:01:22 +0400

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 
 const routes = [
-  { path: "/", name: "Home", component: Home },
+  { path: "", name: "Home", component: Home },
 ];
 
 const router = createRouter({
